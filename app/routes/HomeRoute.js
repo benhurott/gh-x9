5c490ac09899dfffcec39d3cd8b531b55deff8cb
Ben-hur Santos Ott <ben-hur@outlook.com>
2017-04-13 14:07:28 -0300

    add server info

--- app/routes/HomeRoute.js
+++ app/routes/HomeRoute.js
@@ -1,5 +1,14 @@
+var moment = require('moment')
+
 module.exports = function(app) {
     app.get('/', function(req, res, next) {
         res.redirect('/projects');;
     });
+
+	app.get('/server-info', function(req, res, next) {
+		res.status(200).json({
+			now_utc: moment.utc().format(),
+			now_w_timezone: moment().format()
+		});
+	})
 }
